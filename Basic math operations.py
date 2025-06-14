d58b392d9f2de4eedfe6d775ce05c43688baaa8d
Biswajit Dhar <143070495+biswajitdhar76@users.noreply.github.com>
2025-06-14 13:57:18 +0530

    Add files via upload

---
+++ Basic math operations.py
@@ -0,0 +1,10 @@
+
+first_num  = int(input("Enter the first  number: "))
+second_num = int(input("Enter the second number: "))
+
+Addition = first_num + second_num
+Subtraction = first_num - second_num
+Multiplication = first_num * second_num
+Division = first_num / second_num
+
+print( f"Addition : {Addition}\nSubtraction : {Subtraction}\nMultiplication : {Multiplication}\nDivision : {Division}" )
