af270f9c5d5751b423dc0ff7274ef401c7d132f1
Biswajit Dhar <143070495+biswajitdhar76@users.noreply.github.com>
2025-06-14 13:57:40 +0530

    Add files via upload

---
+++ Personalized_Greeting.py
@@ -0,0 +1,5 @@
+first_name= input("Please enter your first name: ")
+last_name = input("Please enter your last name: ")
+
+greeting = f"Hello, {first_name} {last_name}! Welcome to the python program."
+print(greeting)
