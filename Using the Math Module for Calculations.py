975f6d9dd2670b31ed141e72df8f3a4c1873b780
Biswajit Dhar <143070495+biswajitdhar76@users.noreply.github.com>
2025-06-15 22:10:03 +0530

    Add files via upload

---
+++ Using the Math Module for Calculations.py
@@ -0,0 +1,13 @@
+import math
+
+number = int(input("Enter a number: "))
+
+sqroot_result = math.sqrt(number)
+
+log_result = math.log(number)
+
+sine_result = math.sin(number)
+
+print(f"Square root : {sqroot_result}")
+print(f"Logarithm : {log_result}")
+print(f"Sine : {sine_result}")
